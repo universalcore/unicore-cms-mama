dafb43c0aa86f04325c1d3d89a1b16b9ff24365c
Milton Madanda <madandat@gmail.com>
2015-01-24 09:44:43 +0200

    update translations from pootle - bahasa + portuguese

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -4,7 +4,7 @@ mkdir -p unicorecmsmama/locale
 
 pot-create -o unicorecmsmama/locale/unicorecmsmama.pot unicorecmsmama/
 
-declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "swa_KE" "fre_FR" "spa_AR" "spa_CO" "spa_MX" "hin_IN")
+declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "swa_KE" "fre_FR" "spa_AR" "spa_CO" "spa_MX" "hin_IN" "por_PT")
 
 for lang in "${arr[@]}"
 do
