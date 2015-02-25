fa1cc123c716c31cdb7303fa5e68a93844af7652
Rizmari Versfeld <rizziepit@gmail.com>
2015-02-25 13:12:35 +0200

    localise tagline

--- unicorecmsmama/tests/test_views.py
+++ unicorecmsmama/tests/test_views.py
@@ -3,13 +3,34 @@ from pyramid import testing
 
 from cms.tests.base import UnicoreTestCase
 from unicorecmsmama import main
-from unicore.content.models import Page
+from unicore.content.models import Page, Localisation
 
 
 class TestViews(UnicoreTestCase):
 
     def setUp(self):
         self.workspace = self.mk_workspace()
+
+        self.workspace.setup_custom_mapping(Page, {
+            'properties': {
+                'slug': {
+                    'type': 'string',
+                    'index': 'not_analyzed',
+                },
+                'language': {
+                    'type': 'string',
+                }
+            }
+        })
+        self.workspace.setup_custom_mapping(Localisation, {
+            'properties': {
+                'locale': {
+                    'type': 'string',
+                    'index': 'not_analyzed',
+                }
+            }
+        })
+
         settings = {
             'git.path': self.workspace.working_dir,
             'git.content_repo_url': '',
@@ -24,18 +45,6 @@ class TestViews(UnicoreTestCase):
         self.app = self.mk_app(self.workspace, settings=settings, main=main)
 
     def test_homepage_page(self):
-        self.workspace.setup_custom_mapping(Page, {
-            'properties': {
-                'slug': {
-                    'type': 'string',
-                    'index': 'not_analyzed',
-                },
-                'language': {
-                    'type': 'string',
-                }
-            }
-        })
-
         self.create_categories(self.workspace, count=1)
 
         intro_page = Page({
@@ -46,12 +55,42 @@ class TestViews(UnicoreTestCase):
         self.workspace.save(intro_page, 'save intro')
         self.workspace.refresh_index()
 
+        def localise_logo(locale, logo_text):
+            try:
+                [l] = self.workspace.S(Localisation).filter(locale=locale)
+                l = l.get_object()
+                l = l.update({'logo_text': logo_text})
+                self.workspace.save(l, 'Localisation updated')
+                self.workspace.refresh_index()
+            except ValueError:
+                l = self.create_localisation(
+                    self.workspace,
+                    locale=locale,
+                    logo_text=logo_text)
+            resp = self.app.get('/?_LOCALE_=%s' % locale, status=200)
+            return resp.body
+
+        # check default locale
         resp = self.app.get('/', status=200)
         self.assertTrue(
             '<div id="banner">Advice from experts and parents</div>' in
             resp.body)
 
-        resp = self.app.get('/?_LOCALE_=eng_UK', status=200)
+        # check locale without translation
         self.assertTrue(
             '<div id="banner">Advice from experts and parents</div>' in
-            resp.body)
+            localise_logo('eng_GB', None))
+        self.assertTrue(
+            '<div id="banner">Advice from experts and parents</div>' in
+            localise_logo('eng_UK', None))
+        self.assertTrue(
+            '<div id="banner">Advice foo</div>' in
+            localise_logo('eng_GB', 'Advice foo'))
+
+        # check locale with translation
+        self.assertTrue(
+            '<div id="banner">Saran dari pakar dan orang tua</div>' in
+            localise_logo('ind_ID', None))  # falls back to translation files
+        self.assertTrue(
+            '<div id="banner">Advice foo</div>' in
+            localise_logo('ind_ID', 'Advice foo'))
