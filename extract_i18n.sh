c0c54b85c272a760ba253f33c4f176c73d593b3d
Milton Madanda <madandat@gmail.com>
2015-04-14 09:40:16 +0200

    update bengali translations

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -7,7 +7,7 @@ pot-create -o unicorecmsmama/locale/unicorecmsmama.pot unicorecmsmama/
 declare -a arr=(
     "eng_GB" "tha_TH" "ind_ID" "swa_TZ" "swa_KE" "fre_FR" "spa_AR" "spa_CO"
     "spa_MX" "hin_IN" "por_PT" "mal_IN" "guj_IN" "tel_IN" "tam_IN" "mar_IN"
-    "ben_IN" "fil_PH")
+    "ben_IN" "ben_BD" "fil_PH")
 
 for lang in "${arr[@]}"
 do
