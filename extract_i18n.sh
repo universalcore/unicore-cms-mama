1fda46b9c87c4e74153bb0bb14eb9666cca22435
Milton Madanda <madandat@gmail.com>
2015-07-13 09:14:20 +0200

    updated translations

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -7,7 +7,9 @@ pot-create -o unicorecmsmama/locale/unicorecmsmama.pot unicorecmsmama/
 declare -a arr=(
     "eng_GB" "tha_TH" "ind_ID" "swa_TZ" "swa_KE" "fre_FR" "spa_AR" "spa_CO"
     "spa_MX" "hin_IN" "por_PT" "mal_IN" "guj_IN" "tel_IN" "tam_IN" "mar_IN"
-    "ben_IN" "ben_BD" "fil_PH" "urd_PK")
+    "ben_IN" "ben_BD" "fil_PH" "urd_PK" "kan_IN" "pan_IN" "amh_ET" "bur_MM"
+    "tur_TR"
+)
 
 for lang in "${arr[@]}"
 do
